7c7b6ff6fb03e4201fba58283e5866d9272c5c01
YLonely <loneybw@gmail.com>
2021-01-24 08:04:12 +0000

    finish build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 build() {
     name=${1}
-    cd ./$name
-    # build
-    cd ..
+    echo "build with proxy http_proxy=$http_proxy https_proxy=$https_proxy"
+    buildctl build --opt build-arg:http_proxy=$http_proxy --opt build-arg:https_proxy=$https_proxy --frontend=dockerfile.v0 --local context=./$name --local dockerfile=./$name --output type=docker,dest=./images/$name-function.tar,name="$name:function"
 }
 
 function_names=(golang java nodejs python python-ml)
@@ -20,7 +19,7 @@ else
     for name in ${function_names[*]}; do
         if [ $name = $target ]; then
             build $target
-            break
+            exit
         fi
     done
     echo "target $target does not exist"
